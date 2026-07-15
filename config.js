dc853ef39216ae4d69c96907f187cf3552998925
led <vesatukio@gmail.com>
2026-07-15 23:05:53 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+const CONFIG = {
+    API_URL: "https://script.google.com/macros/s/AKfycbwLtAJsbkYAKsx9M1fUJu-eXR2-hMTp7cl2SZrprvGJ0_ql6BWkm9pM-9EBNHXDABGblA/exec",
+    APP_NAME: "DUTA LED ADMIN"
+};
