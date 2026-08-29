b1eacda7adf06994f5a74a0aad40bbd10b0541a7
led <vesatukio@gmail.com>
2026-08-29 14:02:56 +0700

    Configure Duta LED website order API

---
+++ order-config.js
@@ -0,0 +1,2 @@
+/* Set this to the SAME deployed Apps Script Web App URL used by Duta LED. */
+window.ORDER_API = 'https://script.google.com/macros/s/AKfycbyr4eSauu1RneZIrwwPVBilx21kWNrauE9V40D17dmrntqTu4U3OGi4fafAYHXcd-A/exec';
