5c492f4c9c5bdc2cdfdb58557b4cb7ceeedcf3a1
led <vesatukio@gmail.com>
2026-09-17 07:41:35 +0700

    Aktifkan manager varian manual di Admin

--- site-load-fix.js
+++ site-load-fix.js
@@ -1,16 +1,22 @@
-/* DUTA LED - page load helper + watt variant UI loader */
+/* DUTA LED - page load helper + manual variant UI loader */
 (function(){
   'use strict';
-  function loadWattUI(){
+  function loadCatalogVariantUI(){
     if(!document.querySelector('link[data-watt-ui]')){
-      var css=document.createElement('link');css.rel='stylesheet';css.href='watt-variant-group.css?v=1';css.dataset.wattUi='1';document.head.appendChild(css);
+      var css=document.createElement('link');css.rel='stylesheet';css.href='watt-variant-group.css?v=2';css.dataset.wattUi='1';document.head.appendChild(css);
     }
     if(!document.querySelector('script[data-watt-ui]')){
-      var s=document.createElement('script');s.src='watt-variant-group.js?v=1';s.defer=true;s.dataset.wattUi='1';document.body.appendChild(s);
+      var s=document.createElement('script');s.src='watt-variant-group.js?v=2';s.defer=true;s.dataset.wattUi='1';document.body.appendChild(s);
+    }
+  }
+  function loadAdminVariantUI(){
+    if(!document.querySelector('script[data-manual-variant-admin]')){
+      var s=document.createElement('script');s.src='admin-variant-manager.js?v=1';s.defer=true;s.dataset.manualVariantAdmin='1';document.body.appendChild(s);
     }
   }
   function boot(){
-    loadWattUI();
+    if(/admin/i.test(location.pathname)) loadAdminVariantUI();
+    else loadCatalogVariantUI();
     setTimeout(function(){
       const loading=document.getElementById('loading');
       const grid=document.getElementById('productGrid');
